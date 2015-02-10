486d6d2fab8e78ade9b19d7a78d94b76f146b51c
Pine Mizune <pinemz@gmail.com>
2015-02-11 07:35:07 +0900

    Add project information

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject test "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject :4clojure "0.1.0-SNAPSHOT"
+  :description "4clojure"
+  :url "https://github.com/pine613/4clojure"
+  :license {:name "Public Domain"
+            :url "http://unlicense.org/"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :main test_runner.core)
