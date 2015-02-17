95f5c8e9df7e36ef85bdb1794a889fc281b65c17
Pine Mizune <pinemz@gmail.com>
2015-02-17 10:08:45 +0900

    テストのサンプルを追加

--- src/test_runner/core.clj
+++ src/test_runner/core.clj
@@ -1,22 +1,25 @@
 (ns test_runner.core
   (:use [clojure.stacktrace]))
 
+; ファイル名を取得する
+(defn fname [file] (.getName file))
+
 ; ソースファイルのみをフィルタする
 (defn- src-filter [file]
-  (->> file (.getName) (re-matches #"\d+.clj")))
+  (->> file fname (re-matches #"\d+.clj")))
 
 ; テスト対象のソースファイル一覧を取得
 (defn- src-files []
   (let [dirs (clojure.java.io/file "src/4clojure")
         files (file-seq dirs)]
     (let [matched-files (filter src-filter files)]
-      (sort #(compare (.getName %1) (.getName %2)) matched-files))
+      (sort #(compare (fname %1) (fname %2)) matched-files))
   ))
 
 ; ソースファイルをテストする
 (defn- test-file [file]
   (let [path (.getPath file)]
-    (print "Testing " (.getName file) " ... ")
+    (print "Testing " (fname file) " ... ")
     
     (try
       (let [ret (load-file path)]
--- test/test_runner/core_test.clj
+++ test/test_runner/core_test.clj
@@ -1,4 +1,8 @@
 (ns test_runner.core-test
   (:require [clojure.test :refer :all]
-            [test_runner.core :refer :all]))
+            [test_runner.core :refer :all])
+  (:import [java.io File]))
 
+(deftest test_runner-test
+  (testing "should get filename"
+    (is (= (fname (new File "filename")) "filename"))))
